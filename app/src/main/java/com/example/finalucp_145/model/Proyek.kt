397455bc7c42553f40e6156dc2ab8917ae82edb7
nrismawardah <nrismawardah@gmail.com>
2025-01-21 01:18:30 +0700

    Membuat data class untuk Response dan ResponseDetail untuk tabel proyek

--- app/src/main/java/com/example/finalucp_145/model/Proyek.kt
+++ app/src/main/java/com/example/finalucp_145/model/Proyek.kt
@@ -3,6 +3,20 @@ package com.example.finalucp_145.model
 import kotlinx.serialization.Serializable
 
 @Serializable
+data class ProyekResponse(
+    val status: Boolean,
+    val message: String,
+    val data: List<Proyek>
+)
+
+@Serializable
+data class ProyekResponseDetail(
+    val status: Boolean,
+    val message: String,
+    val data: Proyek
+)
+
+@Serializable
 data class Proyek (
     val id_proyek : String,
     val nama_proyek: String,
