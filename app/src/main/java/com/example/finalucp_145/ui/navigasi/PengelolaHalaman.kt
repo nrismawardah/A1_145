7f9f368537206aca4ff227cb7503884bc95a6999
nrismawardah <nrismawardah@gmail.com>
2025-01-23 03:12:25 +0700

    Membuat rute navigasi untuk DestinasiInsertPry

--- app/src/main/java/com/example/finalucp_145/ui/navigasi/PengelolaHalaman.kt
+++ app/src/main/java/com/example/finalucp_145/ui/navigasi/PengelolaHalaman.kt
@@ -9,7 +9,9 @@ import androidx.navigation.compose.rememberNavController
 import com.example.finalucp_145.ui.view.DestinasiSplash
 import com.example.finalucp_145.ui.view.SplashView
 import com.example.finalucp_145.ui.view.proyek.DestinasiHomePry
+import com.example.finalucp_145.ui.view.proyek.DestinasiInsertPry
 import com.example.finalucp_145.ui.view.proyek.HomePryView
+import com.example.finalucp_145.ui.view.proyek.InsertPryView
 
 @Composable
 fun PengelolaHalaman(navController: NavHostController = rememberNavController()){
@@ -33,8 +35,16 @@ fun PengelolaHalaman(navController: NavHostController = rememberNavController())
             route = DestinasiHomePry.route
         ){
             HomePryView(
-                navigateToItemEntry = {},
+                navigateToItemEntry = {navController.navigate(DestinasiInsertPry.route)},
                 onDetailClick = {},
+                onBack = {navController.popBackStack()},
+                navigateToMainMenu = {navController.navigate(DestinasiSplash.route)}
+            )
+        }
+        composable(
+            route = DestinasiInsertPry.route
+        ){
+            InsertPryView(
                 onBack = {navController.popBackStack()},
                 navigateToMainMenu = {navController.navigate(DestinasiSplash.route)}
             )
