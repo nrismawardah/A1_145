8e4f4740aacdd4c660e7a0ee0d95fffc248e912d
nrismawardah <nrismawardah@gmail.com>
2025-01-23 01:42:50 +0700

    Membuat PengelolaHalaman serta membuat rute navigasi untuk DestinasiSplash dan DestinasiHomePry

---
+++ app/src/main/java/com/example/finalucp_145/ui/navigasi/PengelolaHalaman.kt
@@ -0,0 +1,43 @@
+package com.example.finalucp_145.ui.navigasi
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.finalucp_145.ui.view.DestinasiSplash
+import com.example.finalucp_145.ui.view.SplashView
+import com.example.finalucp_145.ui.view.proyek.DestinasiHomePry
+import com.example.finalucp_145.ui.view.proyek.HomePryView
+
+@Composable
+fun PengelolaHalaman(navController: NavHostController = rememberNavController()){
+    NavHost(
+        navController = navController,
+        startDestination = DestinasiSplash.route,
+        modifier = Modifier
+    ){
+        composable(
+            route = DestinasiSplash.route
+        ){
+            SplashView(
+                onProyekClick = {
+                    navController.navigate(DestinasiHomePry.route)
+                },
+                onTimClick = {},
+                onAnggotaClick = {}
+            )
+        }
+        composable(
+            route = DestinasiHomePry.route
+        ){
+            HomePryView(
+                navigateToItemEntry = {},
+                onDetailClick = {},
+                onBack = {navController.popBackStack()},
+                navigateToMainMenu = {navController.navigate(DestinasiSplash.route)}
+            )
+        }
+    }
+}
