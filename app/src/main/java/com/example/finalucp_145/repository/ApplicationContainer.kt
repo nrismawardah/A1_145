49d18236d40b73a3ce503644730765095690b6b1
nrismawardah <nrismawardah@gmail.com>
2025-01-24 17:43:25 +0700

    Mendaftarkan TugasRepository pada AppContainer

--- app/src/main/java/com/example/finalucp_145/repository/ApplicationContainer.kt
+++ app/src/main/java/com/example/finalucp_145/repository/ApplicationContainer.kt
@@ -1,6 +1,7 @@
 package com.example.finalucp_145.repository
 
 import com.example.finalucp_145.service_api.ProyekService
+import com.example.finalucp_145.service_api.TugasService
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import kotlinx.serialization.json.Json
 import okhttp3.MediaType.Companion.toMediaType
@@ -8,6 +9,7 @@ import retrofit2.Retrofit
 
 interface AppContainer {
     val proyekRepository: ProyekRepository
+    val tugasRepository: TugasRepository
 }
 
 class ApplicationContainer : AppContainer {
@@ -19,4 +21,7 @@ class ApplicationContainer : AppContainer {
 
     private val proyekService: ProyekService by lazy { retrofit.create(ProyekService::class.java) }
     override val proyekRepository: ProyekRepository by lazy { NetworkProyekRepository(proyekService) }
+
+    private val tugasService: TugasService by lazy { retrofit.create(TugasService::class.java) }
+    override val tugasRepository: TugasRepository by lazy { NetworkTugasRepository(tugasService) }
 }
