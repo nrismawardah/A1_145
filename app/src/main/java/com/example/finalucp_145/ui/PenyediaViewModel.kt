5af1521b3684062eae8c8b7e8b1ccd9507173797
nrismawardah <nrismawardah@gmail.com>
2025-01-24 17:47:58 +0700

    Mendaftarkan DetailPryViewModel dan HomeTgsViewModel pada PenyediaViewModel

--- app/src/main/java/com/example/finalucp_145/ui/PenyediaViewModel.kt
+++ app/src/main/java/com/example/finalucp_145/ui/PenyediaViewModel.kt
@@ -1,17 +1,26 @@
 package com.example.finalucp_145.ui
 
+
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.finalucp_145.ProjectApplications
+import com.example.finalucp_145.ui.viewmodel.proyek.DetailPryViewModel
 import com.example.finalucp_145.ui.viewmodel.proyek.HomePryViewModel
 import com.example.finalucp_145.ui.viewmodel.proyek.InsertPryViewModel
+import com.example.finalucp_145.ui.viewmodel.tugas.HomeTgsViewModel
 
 object PenyediaViewModel {
     val Factory = viewModelFactory {
+        // Proyek
         initializer { HomePryViewModel(projectApp().container.proyekRepository) }
         initializer { InsertPryViewModel(projectApp().container.proyekRepository) }
+        initializer { DetailPryViewModel(savedStateHandle = createSavedStateHandle(), proyekRepository = projectApp().container.proyekRepository)}
+
+        // Tugas
+        initializer { HomeTgsViewModel(savedStateHandle = createSavedStateHandle(), tugasRepository = projectApp().container.tugasRepository) }
     }
 }
 
