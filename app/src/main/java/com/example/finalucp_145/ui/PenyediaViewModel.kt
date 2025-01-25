96a2b9caeabab1c8a422dc29c77e20edf317d2c9
nrismawardah <nrismawardah@gmail.com>
2025-01-25 17:43:06 +0700

    Mendaftarkan EditPryViewModel pada PenyediaViewModel dan memperbarui DetailPryViewModel dan HomeTgsViewModel

--- app/src/main/java/com/example/finalucp_145/ui/PenyediaViewModel.kt
+++ app/src/main/java/com/example/finalucp_145/ui/PenyediaViewModel.kt
@@ -8,6 +8,7 @@ import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.finalucp_145.ProjectApplications
 import com.example.finalucp_145.ui.viewmodel.proyek.DetailPryViewModel
+import com.example.finalucp_145.ui.viewmodel.proyek.EditPryViewModel
 import com.example.finalucp_145.ui.viewmodel.proyek.HomePryViewModel
 import com.example.finalucp_145.ui.viewmodel.proyek.InsertPryViewModel
 import com.example.finalucp_145.ui.viewmodel.tugas.HomeTgsViewModel
@@ -17,10 +18,11 @@ object PenyediaViewModel {
         // Proyek
         initializer { HomePryViewModel(projectApp().container.proyekRepository) }
         initializer { InsertPryViewModel(projectApp().container.proyekRepository) }
-        initializer { DetailPryViewModel(savedStateHandle = createSavedStateHandle(), proyekRepository = projectApp().container.proyekRepository)}
+        initializer { DetailPryViewModel(createSavedStateHandle(), proyekRepository = projectApp().container.proyekRepository)}
+        initializer { EditPryViewModel(createSavedStateHandle(), proyekRepository = projectApp().container.proyekRepository) }
 
         // Tugas
-        initializer { HomeTgsViewModel(savedStateHandle = createSavedStateHandle(), tugasRepository = projectApp().container.tugasRepository) }
+        initializer { HomeTgsViewModel(createSavedStateHandle(), tugasRepository = projectApp().container.tugasRepository) }
     }
 }
 
