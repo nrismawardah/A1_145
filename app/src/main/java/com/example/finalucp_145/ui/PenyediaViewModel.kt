99c9dcdd0ff0e6fc5a796eba9291db27c0dbb8ce
nrismawardah <nrismawardah@gmail.com>
2025-01-25 23:14:10 +0700

    Mendaftarkan HomeTimViewModel pada PenyediaViewModel

--- app/src/main/java/com/example/finalucp_145/ui/PenyediaViewModel.kt
+++ app/src/main/java/com/example/finalucp_145/ui/PenyediaViewModel.kt
@@ -11,6 +11,7 @@ import com.example.finalucp_145.ui.viewmodel.proyek.DetailPryViewModel
 import com.example.finalucp_145.ui.viewmodel.proyek.EditPryViewModel
 import com.example.finalucp_145.ui.viewmodel.proyek.HomePryViewModel
 import com.example.finalucp_145.ui.viewmodel.proyek.InsertPryViewModel
+import com.example.finalucp_145.ui.viewmodel.tim.HomeTimViewModel
 import com.example.finalucp_145.ui.viewmodel.tugas.DetailTgsViewModel
 import com.example.finalucp_145.ui.viewmodel.tugas.EditTgsViewModel
 import com.example.finalucp_145.ui.viewmodel.tugas.HomeTgsViewModel
@@ -29,6 +30,9 @@ object PenyediaViewModel {
         initializer { InsertTgsViewModel(projectApp().container.tugasRepository) }
         initializer { DetailTgsViewModel(createSavedStateHandle(), tugasRepository = projectApp().container.tugasRepository) }
         initializer { EditTgsViewModel(createSavedStateHandle(), tugasRepository = projectApp().container.tugasRepository) }
+
+        // Tim
+        initializer { HomeTimViewModel(projectApp().container.timRepository) }
     }
 }
 
