e3b13b743fc95712d6ad41cd663c786a0a628571
nrismawardah <nrismawardah@gmail.com>
2025-01-25 17:46:44 +0700

    Membuat rute navigasi untuk DestinasiEditPry

--- app/src/main/java/com/example/finalucp_145/ui/navigasi/PengelolaHalaman.kt
+++ app/src/main/java/com/example/finalucp_145/ui/navigasi/PengelolaHalaman.kt
@@ -11,9 +11,11 @@ import androidx.navigation.navArgument
 import com.example.finalucp_145.ui.view.DestinasiSplash
 import com.example.finalucp_145.ui.view.SplashView
 import com.example.finalucp_145.ui.view.proyek.DestinasiDetailPry
+import com.example.finalucp_145.ui.view.proyek.DestinasiEditPry
 import com.example.finalucp_145.ui.view.proyek.DestinasiHomePry
 import com.example.finalucp_145.ui.view.proyek.DestinasiInsertPry
 import com.example.finalucp_145.ui.view.proyek.DetailPryView
+import com.example.finalucp_145.ui.view.proyek.EditPryView
 import com.example.finalucp_145.ui.view.proyek.HomePryView
 import com.example.finalucp_145.ui.view.proyek.InsertPryView
 import com.example.finalucp_145.ui.view.tugas.DestinasiHomeTgs
@@ -69,7 +71,10 @@ fun PengelolaHalaman(navController: NavHostController = rememberNavController())
             id_proyek?.let {
                 DetailPryView(
                     onBack = {navController.popBackStack()},
-                    onEditClick = { },
+                    onEditClick = { id_proyek ->
+                        navController.navigate("${DestinasiEditPry.route}/$id_proyek")
+                        println(id_proyek)
+                    },
                     navigateToMainMenu = {navController.navigate(DestinasiSplash.route)},
                     navigateToTugas = { proyekId ->
                         navController.navigate("${DestinasiHomeTgs.route}/$proyekId")
@@ -77,6 +82,21 @@ fun PengelolaHalaman(navController: NavHostController = rememberNavController())
                 )
             }
         }
+        composable(
+            route = DestinasiEditPry.routeWithArgs,
+            arguments = listOf(navArgument(DestinasiEditPry.id_proyek){
+                type = NavType.StringType
+            })
+        ){
+            val id_proyek = it.arguments?.getString(DestinasiEditPry.id_proyek)
+            id_proyek?.let {
+                EditPryView(
+                    onBack = {navController.popBackStack()},
+                    navigateToMainMenu = {navController.navigate(DestinasiSplash.route)}
+                )
+            }
+        }
+
         composable(
             route = DestinasiHomeTgs.routeWithArgs,
             arguments = listOf(navArgument(DestinasiHomeTgs.idProyek) {
