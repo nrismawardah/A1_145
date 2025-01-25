9d9faabdc86a1ac485706bc5d5692c4665b9d511
nrismawardah <nrismawardah@gmail.com>
2025-01-25 21:48:11 +0700

    Mendaftarkan EditTgsViewModel pada PenyediaViewModel

--- app/src/main/java/com/example/finalucp_145/ui/PenyediaViewModel.kt
+++ app/src/main/java/com/example/finalucp_145/ui/PenyediaViewModel.kt
@@ -12,6 +12,7 @@ import com.example.finalucp_145.ui.viewmodel.proyek.EditPryViewModel
 import com.example.finalucp_145.ui.viewmodel.proyek.HomePryViewModel
 import com.example.finalucp_145.ui.viewmodel.proyek.InsertPryViewModel
 import com.example.finalucp_145.ui.viewmodel.tugas.DetailTgsViewModel
+import com.example.finalucp_145.ui.viewmodel.tugas.EditTgsViewModel
 import com.example.finalucp_145.ui.viewmodel.tugas.HomeTgsViewModel
 import com.example.finalucp_145.ui.viewmodel.tugas.InsertTgsViewModel
 
@@ -27,6 +28,7 @@ object PenyediaViewModel {
         initializer { HomeTgsViewModel(createSavedStateHandle(), tugasRepository = projectApp().container.tugasRepository) }
         initializer { InsertTgsViewModel(projectApp().container.tugasRepository) }
         initializer { DetailTgsViewModel(createSavedStateHandle(), tugasRepository = projectApp().container.tugasRepository) }
+        initializer { EditTgsViewModel(createSavedStateHandle(), tugasRepository = projectApp().container.tugasRepository) }
     }
 }
 
