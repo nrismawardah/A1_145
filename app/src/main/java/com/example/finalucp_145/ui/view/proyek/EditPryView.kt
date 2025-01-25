bc0f8ceb9502728f24557be01c2ff092013f8c18
nrismawardah <nrismawardah@gmail.com>
2025-01-25 17:41:43 +0700

    Membuat tampilan untuk EditPryView

---
+++ app/src/main/java/com/example/finalucp_145/ui/view/proyek/EditPryView.kt
@@ -0,0 +1,84 @@
+package com.example.finalucp_145.ui.view.proyek
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.finalucp_145.ui.PenyediaViewModel
+import com.example.finalucp_145.ui.navigasi.DestinasiNavigasi
+import com.example.finalucp_145.ui.view.customwidget.TopAppBar
+import com.example.finalucp_145.ui.viewmodel.proyek.EditPryViewModel
+import kotlinx.coroutines.launch
+
+object DestinasiEditPry : DestinasiNavigasi {
+    override val route = "edit_pry"
+    override val titleRes = "Edit Pry"
+    const val id_proyek = "id_proyek"
+    val routeWithArgs = "$route/{$id_proyek}"
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EditPryView(
+    onBack: () -> Unit,
+    navigateToMainMenu: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: EditPryViewModel = viewModel(factory = PenyediaViewModel.Factory)
+){
+    val coroutineScope = rememberCoroutineScope()
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+
+    val uiState = viewModel.uiState
+
+    Scaffold (
+        modifier = modifier.fillMaxSize()
+            .nestedScroll(scrollBehavior.nestedScrollConnection),
+
+        topBar = {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clip(RoundedCornerShape(bottomStart = 25.dp, bottomEnd = 25.dp))
+                    .background(Color(0xffE9DCF7))
+            ){
+                TopAppBar(
+                    judul = "Edit Proyek",
+                    onBack = onBack,
+                    onMainMenuClick = navigateToMainMenu,
+                    onMainMenuConfirm = { navigateToMainMenu() },
+                    modifier = modifier
+                )
+            }
+        }
+    ){ innerPadding ->
+        InsertPryBody(
+            insertUiState = viewModel.uiState,
+            onPryValueChange = viewModel::updateInsertPryState,
+            onSaveClick = {
+                coroutineScope.launch {
+                    viewModel.editProyek()
+                    onBack()
+                }
+            },
+            modifier = modifier
+                .padding(innerPadding)
+                .verticalScroll(rememberScrollState())
+                .fillMaxWidth()
+        )
+    }
+}
