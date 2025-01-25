9b0eed980a4d7da1a40deaa96282c02645f1ee61
nrismawardah <nrismawardah@gmail.com>
2025-01-26 00:56:43 +0700

    Membuat tampilan untuk InsertTimView

---
+++ app/src/main/java/com/example/finalucp_145/ui/view/tim/InsertTimView.kt
@@ -0,0 +1,164 @@
+package com.example.finalucp_145.ui.view.tim
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.finalucp_145.ui.PenyediaViewModel
+import com.example.finalucp_145.ui.navigasi.DestinasiNavigasi
+import com.example.finalucp_145.ui.view.customwidget.TopAppBar
+import com.example.finalucp_145.ui.viewmodel.tim.InsertTimViewModel
+import com.example.finalucp_145.ui.viewmodel.tim.InsertUiEvent
+import com.example.finalucp_145.ui.viewmodel.tim.InsertUiState
+import kotlinx.coroutines.launch
+
+object DestinasiInsertTim : DestinasiNavigasi {
+    override val route = "insert_tim"
+    override val titleRes = "Insert Tim"
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun InsertTimView(
+    onBack: () -> Unit,
+    navigateToMainMenu: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: InsertTimViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {
+    val coroutineScope = rememberCoroutineScope()
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+
+    Scaffold(
+        modifier = modifier
+            .fillMaxSize()
+            .nestedScroll(scrollBehavior.nestedScrollConnection),
+
+        topBar = {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clip(RoundedCornerShape(bottomStart = 25.dp, bottomEnd = 25.dp))
+                    .background(Color(0xffE9DCF7))
+            ) {
+                TopAppBar(
+                    judul = "Tambah Tim",
+                    onBack = onBack,
+                    onMainMenuClick = navigateToMainMenu,
+                    onMainMenuConfirm = { navigateToMainMenu() },
+                    modifier = modifier
+                )
+            }
+        },
+    ) { innerPadding ->
+        InsertTimBody(
+            insertUiState = viewModel.uiState,
+            onTimValueChange = viewModel::updateInsertTimState,
+            onSaveClick = {
+                coroutineScope.launch {
+                    viewModel.insertTim()
+                    onBack()
+                }
+            },
+            modifier = Modifier
+                .padding(innerPadding)
+                .verticalScroll(rememberScrollState())
+                .fillMaxWidth()
+        )
+    }
+}
+
+@Composable
+fun InsertTimBody(
+    insertUiState: InsertUiState,
+    onTimValueChange: (InsertUiEvent) -> Unit,
+    onSaveClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        verticalArrangement = Arrangement.spacedBy(16.dp),
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(20.dp)
+    ) {
+        FormInsertTim(
+            insertUiEvent = insertUiState.insertUiEvent,
+            onValueChange = onTimValueChange,
+            modifier = Modifier
+                .fillMaxWidth()
+        )
+        Button(
+            onClick = onSaveClick,
+            shape = MaterialTheme.shapes.small,
+            modifier = Modifier
+                .fillMaxWidth(),
+            colors = androidx.compose.material3.ButtonDefaults.buttonColors(
+                containerColor = Color(0xff3aaffe)
+            )
+        ) {
+            Text(text = "Simpan")
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun FormInsertTim(
+    insertUiEvent: InsertUiEvent,
+    modifier: Modifier = Modifier,
+    onValueChange: (InsertUiEvent) -> Unit = {},
+    enabled: Boolean = true
+) {
+
+    Column(
+        modifier = modifier
+            .fillMaxWidth(),
+        verticalArrangement = Arrangement.spacedBy(10.dp)
+    ) {
+        OutlinedTextField(
+            value = insertUiEvent.id_tim,
+            onValueChange = { onValueChange(insertUiEvent.copy(id_tim = it)) },
+            label = { Text("ID Tim") },
+            modifier = Modifier
+                .fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+        OutlinedTextField(
+            value = insertUiEvent.nama_tim,
+            onValueChange = { onValueChange(insertUiEvent.copy(nama_tim = it)) },
+            label = { Text("Nama Tim") },
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+        OutlinedTextField(
+            value = insertUiEvent.deskripsi_tim,
+            onValueChange = { onValueChange(insertUiEvent.copy(deskripsi_tim = it)) },
+            label = { Text("Deskripsi Tim") },
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+    }
+}
