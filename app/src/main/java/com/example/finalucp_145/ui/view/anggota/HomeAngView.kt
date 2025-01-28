f52d6617ae16b81b4f17f97273bf6f9bfbae3f48
nrismawardah <nrismawardah@gmail.com>
2025-01-28 20:56:53 +0700

    Membuat tampilan untuk HomeAngView

---
+++ app/src/main/java/com/example/finalucp_145/ui/view/anggota/HomeAngView.kt
@@ -0,0 +1,291 @@
+package com.example.finalucp_145.ui.view.anggota
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.finalucp_145.R
+import com.example.finalucp_145.model.Anggota
+import com.example.finalucp_145.ui.PenyediaViewModel
+import com.example.finalucp_145.ui.navigasi.DestinasiNavigasi
+import com.example.finalucp_145.ui.view.customwidget.TopAppBar
+import com.example.finalucp_145.ui.viewmodel.anggota.HomeAngUiState
+import com.example.finalucp_145.ui.viewmodel.anggota.HomeAngViewModel
+
+object DestinasiHomeAng : DestinasiNavigasi {
+    override val route = "home_ang"
+    override val titleRes = "Home Ang"
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeAngView(
+    navigateToItemEntry: () -> Unit,
+    navigateToMainMenu: () -> Unit,
+    modifier: Modifier = Modifier,
+    onDetailClick: (String) -> Unit = {},
+    onBack: () -> Unit,
+    viewModel: HomeAngViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+
+    val showDialog = remember { mutableStateOf(false) }
+    val angToDelete = remember { mutableStateOf<Anggota?>(null) }
+
+    LaunchedEffect(Unit) {
+        viewModel.getAng()
+    }
+
+    Scaffold(
+        modifier = modifier,
+        topBar = {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clip(RoundedCornerShape(bottomStart = 25.dp, bottomEnd = 25.dp))
+                    .background(Color.White)
+            ) {
+                TopAppBar(
+                    judul = "Daftar Anggota",
+                    onBack = onBack,
+                    onMainMenuClick = navigateToMainMenu,
+                    onMainMenuConfirm = { navigateToMainMenu() },
+                    modifier = modifier
+                )
+            }
+        },
+        containerColor = Color(0xffE9DCF7),
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = navigateToItemEntry,
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.padding(18.dp),
+                containerColor = Color(0xfffdc938)
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Add,
+                    contentDescription = "Add Anggota"
+                )
+            }
+        }
+    ) { innerPadding ->
+        HomeAngStatus(
+            homeAngUiState = viewModel.angUiState,
+            retryAction = { viewModel.getAng() },
+            modifier = Modifier.padding(innerPadding),
+            onDetailClick = onDetailClick,
+            onDeleteClick = { ang ->
+                angToDelete.value = ang
+                showDialog.value = true
+            }
+        )
+    }
+
+    if (showDialog.value && angToDelete.value != null) {
+        AlertDialog(
+            onDismissRequest = { showDialog.value = false },
+            title = {
+                Text(
+                    text = "Konfirmasi Penghapusan",
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(vertical = 8.dp),
+                    textAlign = androidx.compose.ui.text.style.TextAlign.Center
+                )
+            },
+            text = {
+                Text(
+                    text = "Apakah Anda yakin ingin menghapus anggota '${angToDelete.value?.nama_anggota}'?",
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(vertical = 8.dp),
+                    textAlign = androidx.compose.ui.text.style.TextAlign.Center
+                )
+            },
+            confirmButton = {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 16.dp),
+                    horizontalArrangement = Arrangement.SpaceEvenly
+                ) {
+                    Button(
+                        onClick = {
+                            angToDelete.value?.let {
+                                viewModel.deleteAng(it.id_anggota)
+                                viewModel.getAng()
+                            }
+                            showDialog.value = false
+                        },
+                        colors = ButtonDefaults.buttonColors(containerColor = Color(0xff3aaffe))
+                    ) {
+                        Text("Ya", color = Color.White)
+                    }
+                    Button(
+                        onClick = { showDialog.value = false },
+                        colors = ButtonDefaults.buttonColors(containerColor = Color(0xff3aaffe))
+                    ) {
+                        Text("Tidak", color = Color.White)
+                    }
+                }
+            },
+            dismissButton = {}
+        )
+    }
+}
+
+@Composable
+fun HomeAngStatus(
+    homeAngUiState: HomeAngUiState,
+    retryAction: () -> Unit,
+    modifier: Modifier = Modifier,
+    onDeleteClick: (Anggota) -> Unit = {},
+    onDetailClick: (String) -> Unit
+) {
+    when (homeAngUiState) {
+        is HomeAngUiState.Loading -> OnLoading(modifier = modifier.fillMaxSize())
+
+        is HomeAngUiState.Success -> {
+            if (homeAngUiState.anggota.isEmpty()) {
+                Box(modifier = modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                    Text(text = "Tidak ada data anggota")
+                }
+            } else {
+                AnggotaLayout(
+                    anggota = homeAngUiState.anggota,
+                    modifier = modifier.fillMaxWidth(),
+                    onDetailClick = { onDetailClick(it.id_anggota) },
+                    onDeleteClick = { onDeleteClick(it) }
+                )
+            }
+        }
+        is HomeAngUiState.Error -> OnError(retryAction, modifier = modifier.fillMaxSize())
+    }
+}
+
+@Composable
+fun OnLoading(modifier: Modifier = Modifier) {
+    Image(
+        modifier = modifier.size(20.dp),
+        painter = painterResource(R.drawable.loading_img),
+        contentDescription = stringResource(R.string.loading)
+    )
+}
+
+@Composable
+fun OnError(retryAction: () -> Unit, modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.ic_connection_error),
+            contentDescription = "",
+        )
+        Text(text = stringResource(R.string.loading_failed), modifier = Modifier.padding(16.dp))
+        Button(onClick = retryAction) {
+            Text(stringResource(R.string.retry))
+        }
+    }
+}
+
+@Composable
+fun AnggotaLayout(
+    anggota: List<Anggota>,
+    modifier: Modifier = Modifier,
+    onDetailClick: (Anggota) -> Unit,
+    onDeleteClick: (Anggota) -> Unit = {}
+) {
+    LazyColumn(
+        modifier = modifier,
+        contentPadding = PaddingValues(20.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        items(anggota) { anggota ->
+            AnggotaCard(
+                anggota = anggota,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 10.dp)
+                    .clickable { onDetailClick(anggota) },
+                onDeleteClick = { onDeleteClick(anggota) }
+            )
+        }
+    }
+}
+
+@Composable
+fun AnggotaCard(
+    anggota: Anggota,
+    modifier: Modifier = Modifier,
+    onDeleteClick: (Anggota) -> Unit = {}
+) {
+    Card(
+        modifier = modifier,
+        shape = MaterialTheme.shapes.medium,
+        elevation = CardDefaults.cardElevation(defaultElevation = 8.dp)
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(Color.White)
+                .padding(16.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(horizontal = 16.dp),
+                verticalArrangement = Arrangement.spacedBy(8.dp)
+            ) {
+                Text(
+                    text = anggota.nama_anggota,
+                    style = MaterialTheme.typography.titleLarge,
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.SemiBold
+                )
+                Divider(
+                    color = Color(0xffE9DCF7),
+                    thickness = 1.dp,
+                    modifier = Modifier.padding(vertical = 4.dp)
+                )
+                Text(
+                    text = anggota.peran,
+                    style = MaterialTheme.typography.titleMedium
+                )
+                Text(
+                    text = anggota.id_tim,
+                    style = MaterialTheme.typography.titleMedium
+                )
+            }
+            IconButton(onClick = { onDeleteClick(anggota) }) {
+                Icon(
+                    imageVector = Icons.Default.Delete,
+                    contentDescription = null,
+                    modifier = Modifier.size(30.dp),
+                    tint = Color(0xfffe8b18)
+                )
+            }
+        }
+    }
+}
